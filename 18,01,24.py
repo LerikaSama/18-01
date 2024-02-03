68339a8ae7ffd1d2d7434ba191f288add7823d88
LerikaSama <152898262+LerikaSama@users.noreply.github.com>
2024-02-03 13:59:30 +0300

    Add files via upload

---
+++ 18,01,24.py
@@ -0,0 +1,32 @@
+#Задание 1
+
+#a = input("Введите строку для проверки:")
+#if a != a[::-1]:
+#    print("It's not palindrome")
+#else:
+#    print("It's palindrome")
+#input()
+
+
+#Задание 2
+
+#text = input("Введите текст:")
+#reserv_words = input("Введите резервный список зарезервированных слов:")
+
+#words = text.split()
+
+#for i in range(len(words)):
+#    if words[i] in reserv_words:
+#        words[i] = words[i].upper()
+
+#modif_text = " ".join(words)
+
+#print("Измененный текст^")
+#print(modif_text)
+
+
+
+#Задание 3
+
+
+print(len(input("Введите текст для проверки:").split('.')))
